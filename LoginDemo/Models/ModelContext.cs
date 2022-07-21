14fd0e7e5a201a1066c22ac356e5f992e946c8b7
VenusHui <2913727619@qq.com>
2022-07-21 20:09:40 +0800

    Create LoginDemo

---
+++ LoginDemo/Models/ModelContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace LoginDemo.Models
+{
+    public partial class ModelContext : DbContext
+    {
+        public ModelContext()
+        {
+        }
+
+        public ModelContext(DbContextOptions<ModelContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseOracle("User Id=admin;Password=admin123456;Data Source=106.14.106.243:1521/xe;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:DefaultSchema", "ADMIN");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.UserName)
+                    .HasName("SYS_C0010004");
+
+                entity.Property(e => e.UserName)
+                    .HasColumnName("user_name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
