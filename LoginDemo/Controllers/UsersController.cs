afac8189054ea88c5f9ae37c15de7a2181ae88d9
sayroll <83714631+sayroll@users.noreply.github.com>
2022-07-22 14:34:46 +0800

    LoginDemo
    web api for LoginDemo polished
    make JsonResult be the type of return value

---
+++ LoginDemo/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LoginDemo.Models;
+
+namespace LoginDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ModelContext _context;
+
+        public UsersController(ModelContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public JsonResult Login(string name, string password)
+        {
+            IQueryable<User> users = _context.User;
+            users = users.Where(u => u.UserName == name);
+            List<User> sutiUsers = users.ToList();
+            if (sutiUsers.Count() == 1 && sutiUsers.First().Password == password)
+            {
+                return new JsonResult(new Message
+                {
+                    Status = Message.STATUS_SUCCESS,
+                    Reply = "登录成功",
+                });
+            }
+            else
+            {
+                return  new JsonResult(new Message
+                {
+                    Status = Message.STATUS_ERROR,
+                    Reply = "请检查用户名或密码输入是否正确"
+                }); ;
+            }
+        }
+
+        [HttpGet]
+        public JsonResult Register(string name, string password)
+        {
+            IQueryable<User> users = _context.User;
+            users = users.Where(u => u.UserName == name);
+            List<User> sutiUsers = users.ToList();
+            if (sutiUsers.Count() > 0)
+            {
+                return new JsonResult(new Message
+                {
+                    Status = Message.STATUS_ERROR,
+                    Reply = "用户名已被注册，请选择一个新的用户名",
+                });
+            }
+            User newUser = new User { UserName = name, Password = password };
+            _context.User.Add(newUser);
+            _context.SaveChanges();
+            return new JsonResult(new Message
+            {
+                Status = Message.STATUS_SUCCESS,
+                Reply = "注册成功",
+            });
+        }
+
+    }
+}
---
+++ LoginDemo/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginDemo.Models
+{
+    public class Message
+    {
+        //错误状态标识
+        public static string STATUS_ERROR = "0";
+
+        //正确状态标识
+        public static string STATUS_SUCCESS = "1";
+        private string status;//状态，成果或失败
+        private string reply;//返回的提示消息
+
+        public string Status { get => status; set => status = value; }
+        public string Reply { get => reply; set => reply = value; }
+
+    }
+}
