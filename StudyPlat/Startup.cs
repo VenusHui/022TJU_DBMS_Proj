35e6a65126e0e97189d9687689460838767eec2d
VenusHui <2913727619@qq.com>
2022-08-10 17:11:30 +0800

    Update swagger

--- StudyPlat/Startup.cs
+++ StudyPlat/Startup.cs
@@ -4,14 +4,21 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 
-using StudyPlat.Entitys;
+using StudyPlat.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using StudyPlat.Jwt;
+using StudyPlat.IdentityServer;
 
 namespace StudyPlat
 {
@@ -19,6 +26,12 @@ namespace StudyPlat
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             // 跨域方法，先注入服务，声明策略，然后再下边app中配置开启中间件
@@ -45,42 +58,65 @@ namespace StudyPlat
             {
                 c.SwaggerDoc("v1.0", new OpenApiInfo()
                 {
-                    Title = "LoginDemo",
+                    Title = "swagger",
                     Version = "v1.0"
                 });
             });
+
+            //token的配置
+            services.Configure<TokenModel>(Configuration.GetSection("Authentication"));
+            var token = Configuration.GetSection("Authentication").Get<TokenModel>();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(option =>
+            {
+                option.RequireHttpsMetadata = false;
+                option.SaveToken = true;
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(token.Secret)),
+                    ValidIssuer = token.Issuer,
+                    ValidAudience = token.Audience,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddScoped<IAuthenticateService, AuthenticateService>();
+            services.AddScoped<User>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // 跨域服务中间件
-            app.UseCors("any"); // 开创！
-
-            // Swagger服务中间件
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "StudyPlat");
-            });
-
+            app.UseCors("any");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "StudyPlat");
+                });
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                //endpoints.MapGet("/", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello World!");
-                //});
             });
+
         }
     }
 }
